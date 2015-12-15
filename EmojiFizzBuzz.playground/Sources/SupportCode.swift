2e47596e898fb91b9a79e34b18e201bd56c78da7
Joakim Gyllstrom <joakim@backslashed.se>
2015-12-15 14:06:05 +0100

    Added emoji fizz buzz playground

---
+++ EmojiFizzBuzz.playground/Sources/SupportCode.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+public let 👍 = true
+public let 👎 = false
+public let 💤 = "Fizz"
+public let 🐝 = "Buzz"
+public let 💩 = 0
+public let 👋 = 5
+public let 👪 = 3
+
+public typealias 📝 = String
+public typealias 🔢 = Int
